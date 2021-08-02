68659b025224629181840c68aa1b9e36345821ee
nero-15 <nero15.dev@gmail.com>
2021-08-02 22:38:23 +0900

    object type

--- Handbook/everyday-types.js
+++ Handbook/everyday-types.js
@@ -7,3 +7,9 @@ function getFavoriteNumber() {
     return 26;
 }
 console.log(getFavoriteNumber());
+// The parameter's type annotation is an object type
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
