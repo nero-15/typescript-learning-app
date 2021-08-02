64e5bdb765b4b05f38fa53a2ff6dad99a5ffd2ff
nero-15 <nero15.dev@gmail.com>
2021-08-02 22:31:27 +0900

    return number

--- Handbook/types.js
+++ Handbook/types.js
@@ -3,3 +3,7 @@ function greet(name) {
     console.log("Hello, " + name.toUpperCase() + "!!");
 }
 greet('toka');
+function getFavoriteNumber() {
+    return 26;
+}
+console.log(getFavoriteNumber());
--- Handbook/types.ts
+++ Handbook/types.ts
@@ -2,6 +2,9 @@
 function greet(name: string) {
   console.log("Hello, " + name.toUpperCase() + "!!");
 }
-
-// Would be a runtime error if executed!
 greet('toka');
+
+function getFavoriteNumber(): number {
+  return 26;
+}
+console.log(getFavoriteNumber());
