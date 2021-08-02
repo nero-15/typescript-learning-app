5674a6f7035cd045734c8963d769c73422c6201b
nero-15 <nero15.dev@gmail.com>
2021-08-02 22:47:42 +0900

    union type

--- Handbook/everyday-types.js
+++ Handbook/everyday-types.js
@@ -23,3 +23,12 @@ function printName(obj) {
 // Both OK
 printName({ first: "Bob" });
 printName({ first: "Alice", last: "Alisson" });
+function printId(id) {
+    console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+// OK
+printId("202");
+// Error
+//printId({ myID: 22342 });
--- Handbook/everyday-types.ts
+++ Handbook/everyday-types.ts
@@ -27,3 +27,13 @@ function printName(obj: { first: string; last?: string }) {
 // Both OK
 printName({ first: "Bob" });
 printName({ first: "Alice", last: "Alisson" });
+
+function printId(id: number | string) {
+  console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+// OK
+printId("202");
+// Error
+//printId({ myID: 22342 });
