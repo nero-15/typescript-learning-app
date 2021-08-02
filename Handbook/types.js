b5ce35221c3352fd0feb6e2110b4f75fc55f71b9
nero-15 <nero15.dev@gmail.com>
2021-08-02 22:29:17 +0900

    types

---
+++ Handbook/types.js
@@ -0,0 +1,6 @@
+// Parameter type annotation
+function greet(name) {
+    console.log("Hello, " + name.toUpperCase() + "!!");
+}
+// Would be a runtime error if executed!
+greet(42);
---
+++ Handbook/types.ts
@@ -0,0 +1,7 @@
+// Parameter type annotation
+function greet(name: string) {
+  console.log("Hello, " + name.toUpperCase() + "!!");
+}
+
+// Would be a runtime error if executed!
+greet(42);
