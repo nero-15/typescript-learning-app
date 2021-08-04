62243fe2a2f0f0cf394f8237b8bff956cf58b54a
nero-15 <nero15.dev@gmail.com>
2021-08-04 21:40:59 +0900

    fix

--- Handbook/narrowing.js
+++ Handbook/narrowing.js
@@ -5,3 +5,15 @@ function padLeft(padding, input) {
     return padding + input;
 }
 console.log(padLeft(5, 'backnumber'));
+function example(x, y) {
+    if (x === y) {
+        // We can now call any 'string' method on 'x' or 'y'.
+        x.toUpperCase();
+        y.toLowerCase();
+    }
+    else {
+        console.log(x);
+        console.log(y);
+    }
+}
+example('aaaa', 'aaaa');
--- Handbook/narrowing.ts
+++ Handbook/narrowing.ts
@@ -6,3 +6,16 @@ function padLeft(padding: number | string, input: string) {
 }
 
 console.log(padLeft(5, 'backnumber'));
+
+function example(x: string | number, y: string | boolean) {
+  if (x === y) {
+    // We can now call any 'string' method on 'x' or 'y'.
+    x.toUpperCase();
+    y.toLowerCase();
+  } else {
+    console.log(x);
+    console.log(y);
+  }
+ }
+
+example('aaaa', 'aaaa')
