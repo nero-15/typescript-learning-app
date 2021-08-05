917bec9104ca68b27ee0e1a113e8e78342262558
nero-15 <nero15.dev@gmail.com>
2021-08-05 23:46:46 +0900

    Call Signatures

--- Handbook/functions.ts
+++ Handbook/functions.ts
@@ -9,3 +9,11 @@ function printToConsole(s: string) {
 }
 
 greeter(printToConsole);
+
+type DescribableFunction = {
+  description: string;
+  (someArg: number): boolean;
+};
+function doSomething(fn: DescribableFunction) {
+  console.log(fn.description + " returned " + fn(6));
+}
