37f6a46cdb7953ef8302e7b052021e9c1202299b
nero-15 <nero15.dev@gmail.com>
2021-08-07 22:12:51 +0900

    readonly

--- Handbook/object-types.ts
+++ Handbook/object-types.ts
@@ -12,3 +12,15 @@ let toka: Person = {
     age: 1
 }
 console.log(greet(toka))
+
+interface SomeType {
+  readonly prop: string;
+}
+
+function doSomething(obj: SomeType) {
+  // We can read from 'obj.prop'.
+  console.log(`prop has the value '${obj.prop}'.`);
+
+  // But we can't re-assign it.
+  //obj.prop = "hello";
+}
