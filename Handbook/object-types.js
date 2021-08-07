ef63607322369a9b26a674a912963392ee26829f
nero-15 <nero15.dev@gmail.com>
2021-08-07 22:14:40 +0900

    fix

--- Handbook/object-types.js
+++ Handbook/object-types.js
@@ -6,3 +6,18 @@ var toka = {
     age: 1
 };
 console.log(greet(toka));
+function doSomething(obj) {
+    // We can read from 'obj.prop'.
+    console.log("prop has the value '" + obj.prop + "'.");
+    // But we can't re-assign it.
+    //obj.prop = "hello";
+}
+var writablePerson = {
+    name: "Person McPersonface",
+    age: 42
+};
+// works
+var readonlyPerson = writablePerson;
+console.log(readonlyPerson.age); // prints '42'
+writablePerson.age++;
+console.log(readonlyPerson.age); // prints '43'
--- Handbook/object-types.ts
+++ Handbook/object-types.ts
@@ -24,3 +24,25 @@ function doSomething(obj: SomeType) {
   // But we can't re-assign it.
   //obj.prop = "hello";
 }
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface ReadonlyPerson {
+  readonly name: string;
+  readonly age: number;
+}
+
+let writablePerson: Person = {
+  name: "Person McPersonface",
+  age: 42,
+};
+
+// works
+let readonlyPerson: ReadonlyPerson = writablePerson;
+
+console.log(readonlyPerson.age); // prints '42'
+writablePerson.age++;
+console.log(readonlyPerson.age); // prints '43'
