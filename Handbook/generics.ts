464471eeff03b4399da7cb99625730d315ff1422
nero-15 <nero15.dev@gmail.com>
2021-08-08 13:19:44 +0900

    GenericIdentityFn

--- Handbook/generics.ts
+++ Handbook/generics.ts
@@ -1,3 +1,7 @@
+interface GenericIdentityFn {
+  <Type>(arg: Type): Type;
+}
+
 function identity<Type>(arg: Type): Type {
     //console.log(arg.length);//error
     return arg;
@@ -10,3 +14,5 @@ function loggingIdentity<Type>(arg: Type[]): Type[] {
   console.log(arg.length);// Array has a .length, so no more error
   return arg;
 }
+
+let myIdentity: GenericIdentityFn = identity;
