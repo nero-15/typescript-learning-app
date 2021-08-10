91dbb3e3143aba327d87ed56671fefc1fb282974
nero-15 <nero15.dev@gmail.com>
2021-08-10 22:05:28 +0900

    Mapping Modifiers

--- Handbook/mapped_types.ts
+++ Handbook/mapped_types.ts
@@ -20,3 +20,28 @@ type FeatureFlags = {
 };
  
 type FeatureOptions = OptionsFlags<FeatureFlags>;
+
+// Removes 'readonly' attributes from a type's properties
+type CreateMutable<Type> = {
+  -readonly [Property in keyof Type]: Type[Property];
+};
+ 
+type LockedAccount = {
+  readonly id: string;
+  readonly name: string;
+};
+ 
+type UnlockedAccount = CreateMutable<LockedAccount>;
+
+// Removes 'optional' attributes from a type's properties
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+ 
+type MaybeUser = {
+  id: string;
+  name?: string;
+  age?: number;
+};
+ 
+type User = Concrete<MaybeUser>;
