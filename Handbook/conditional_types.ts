71fa0877915a3d1fe97b1c552b66bf37e609ef1f
nero-15 <nero15.dev@gmail.com>
2021-08-10 21:54:27 +0900

    conditional types

---
+++ Handbook/conditional_types.ts
@@ -0,0 +1,9 @@
+interface Animal {
+  live(): void;
+}
+interface Dog extends Animal {
+  woof(): void;
+}
+ 
+type Example1 = Dog extends Animal ? number : string; 
+type Example2 = RegExp extends Animal ? number : string;
