8e7cb18a479fe1b2b12abd0d521f948931ad6dd8
nero-15 <nero15.dev@gmail.com>
2021-08-12 21:12:09 +0900

    constructor

--- Handbook/classes.js
+++ Handbook/classes.js
@@ -1,13 +1,26 @@
-var Point = /** @class */ (function () {
-    function Point() {
-        this.x = 0;
-        this.y = 0;
-    }
-    return Point;
-}());
-var pt = new Point();
+// class Point {
+//   x = 0;
+//   y = 0;
+// }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// const pt = new Point();
 // Prints 0, 0
-console.log(pt.x + ", " + pt.y);
+// console.log(`${pt.x}, ${pt.y}`);
+//
 // pt.x = "0"; error
 var GoodGreeter = /** @class */ (function () {
     function GoodGreeter() {
@@ -39,3 +52,34 @@ var Greeter = /** @class */ (function () {
 var g = new Greeter();
 //g.name = "also not ok";
 console.log(g);
+var Point = /** @class */ (function () {
+    // Normal signature with defaults
+    function Point(x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        this.x = x;
+        this.y = y;
+    }
+    return Point;
+}());
+var pt = new Point();
+console.log(pt);
+var Base = /** @class */ (function () {
+    function Base() {
+        this.k = 4;
+    }
+    return Base;
+}());
+var Derived = /** @class */ (function (_super) {
+    __extends(Derived, _super);
+    function Derived() {
+        var _this = 
+        // Prints a wrong value in ES5; throws exception in ES6
+        _super.call(this) || this;
+        console.log(_this.k);
+        return _this;
+    }
+    return Derived;
+}(Base));
+var derived = new Derived();
+console.log(derived);
--- Handbook/classes.ts
+++ Handbook/classes.ts
@@ -1,12 +1,12 @@
-class Point {
-  x = 0;
-  y = 0;
-}
+// class Point {
+//   x = 0;
+//   y = 0;
+// }
 
-const pt = new Point();
+// const pt = new Point();
 // Prints 0, 0
-console.log(`${pt.x}, ${pt.y}`);
-
+// console.log(`${pt.x}, ${pt.y}`);
+//
 // pt.x = "0"; error
 
 class GoodGreeter  {
@@ -43,3 +43,31 @@ class Greeter {
 const g = new Greeter();
 //g.name = "also not ok";
 console.log(g);
+
+class Point {
+  x: number;
+  y: number;
+
+  // Normal signature with defaults
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
+const pt = new Point();
+console.log(pt);
+
+class Base {
+  k = 4;
+}
+
+class Derived extends Base {
+  constructor() {
+    // Prints a wrong value in ES5; throws exception in ES6
+    super();//'super' must be called before accessing 'this' in the constructor of a derived class.
+    console.log(this.k);
+  }
+}
+
+const derived = new Derived();
+console.log(derived);
