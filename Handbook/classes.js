47d37c0ee991846fb916d6a5c115cf04431afc85
nero-15 <nero15.dev@gmail.com>
2021-08-12 21:04:55 +0900

    fields

--- Handbook/classes.js
+++ Handbook/classes.js
@@ -8,3 +8,19 @@ var Point = /** @class */ (function () {
 var pt = new Point();
 // Prints 0, 0
 console.log(pt.x + ", " + pt.y);
+// pt.x = "0"; error
+var GoodGreeter = /** @class */ (function () {
+    function GoodGreeter() {
+        this.name = "hello";
+    }
+    return GoodGreeter;
+}());
+var gg = new GoodGreeter();
+console.log(gg);
+var OKGreeter = /** @class */ (function () {
+    function OKGreeter() {
+    }
+    return OKGreeter;
+}());
+var og = new OKGreeter();
+console.log(og);
--- Handbook/classes.ts
+++ Handbook/classes.ts
@@ -8,3 +8,21 @@ const pt = new Point();
 console.log(`${pt.x}, ${pt.y}`);
 
 // pt.x = "0"; error
+
+class GoodGreeter  {
+  name: string;
+
+  constructor() {
+    this.name = "hello";
+  }
+}
+
+const gg = new GoodGreeter ();
+console.log(gg);
+
+class OKGreeter {
+  // Not initialized, but no error
+  name!: string;
+}
+const og = new OKGreeter ();
+console.log(og);
