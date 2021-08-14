b9b5f12df0cf2f706cffc9080d5f3f26da787fe4
nero-15 <nero15.dev@gmail.com>
2021-08-14 13:27:08 +0900

    fix

--- Handbook/modules.js
+++ Handbook/modules.js
@@ -2,3 +2,6 @@
 exports.__esModule = true;
 var hello_js_1 = require("./hello.js");
 hello_js_1["default"]();
+var maths_js_1 = require("./maths.js");
+console.log(maths_js_1.pi);
+var absPhi = maths_js_1.absolute(maths_js_1.phi);
--- Handbook/modules.ts
+++ Handbook/modules.ts
@@ -1,2 +1,7 @@
 import hello from "./hello.js";
 hello();
+
+import { pi, phi, absolute } from "./maths.js";
+
+console.log(pi);
+const absPhi = absolute(phi);
