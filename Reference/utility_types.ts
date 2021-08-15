82bff53bdaa0c09a7dbd55ca13e10f2b897f587c
nero-15 <nero15.dev@gmail.com>
2021-08-15 14:49:18 +0900

    Extract<Type, Union>

--- Reference/utility_types.ts
+++ Reference/utility_types.ts
@@ -93,3 +93,10 @@ type T1 = Exclude<"a" | "b" | "c", "a" | "b">;
 type T2 = Exclude<string | number | (() => void), Function>;
 
 // type T2 = string | number
+
+type T3 = Extract<"a" | "b" | "c", "a" | "f">;
+
+// type T3 = "a"
+type T4 = Extract<string | number | (() => void), Function>;
+
+// type T4 = () => void
