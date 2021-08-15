7ca140bf7f48cdff17c4c93a2b2c2d40bec65752
nero-15 <nero15.dev@gmail.com>
2021-08-15 14:48:23 +0900

    Exclude<Type, ExcludedUnion>

--- Reference/utility_types.ts
+++ Reference/utility_types.ts
@@ -83,3 +83,13 @@ const todo: TodoPreview = {
 };
 
 console.log(todo)
+
+type T0 = Exclude<"a" | "b" | "c", "a">;
+
+// type T0 = "b" | "c"
+type T1 = Exclude<"a" | "b" | "c", "a" | "b">;
+
+// type T1 = "c"
+type T2 = Exclude<string | number | (() => void), Function>;
+
+// type T2 = string | number
