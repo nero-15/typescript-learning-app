6575c2e91a44cf1f62b3cbb8d9368f4986c64967
nero-15 <nero15.dev@gmail.com>
2021-08-15 14:41:28 +0900

    Record<Keys,Type>

--- Reference/utility_types.js
+++ Reference/utility_types.js
@@ -1,23 +1,32 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+// interface Todo {
+//   title: string;
+//   description: string;
+// }
+//
+// function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+//   return { ...todo, ...fieldsToUpdate };
+// }
+//
+// const todo1 = {
+//   title: "organize desk",
+//   description: "clear clutter",
+// };
+//
+// const todo2 = updateTodo(todo1, {
+//   description: "throw out trash",
+// });
+//
+// console.log(todo1);
+// console.log(todo2);
+var obj = { a: 5 };
+var todo = {
+    title: "Delete inactive users"
 };
-function updateTodo(todo, fieldsToUpdate) {
-    return __assign(__assign({}, todo), fieldsToUpdate);
-}
-var todo1 = {
-    title: "organize desk",
-    description: "clear clutter"
+// todo.title = "Hello";
+console.log(todo);
+var cats = {
+    miffy: { age: 10, breed: "Persian" },
+    boris: { age: 5, breed: "Maine Coon" },
+    mordred: { age: 16, breed: "British Shorthair" }
 };
-var todo2 = updateTodo(todo1, {
-    description: "throw out trash"
-});
-console.log(todo1);
-console.log(todo2);
+console.log(cats.boris);
--- Reference/utility_types.ts
+++ Reference/utility_types.ts
@@ -1,23 +1,23 @@
-interface Todo {
-  title: string;
-  description: string;
-}
-
-function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
-  return { ...todo, ...fieldsToUpdate };
-}
-
-const todo1 = {
-  title: "organize desk",
-  description: "clear clutter",
-};
-
-const todo2 = updateTodo(todo1, {
-  description: "throw out trash",
-});
-
-console.log(todo1);
-console.log(todo2);
+// interface Todo {
+//   title: string;
+//   description: string;
+// }
+//
+// function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+//   return { ...todo, ...fieldsToUpdate };
+// }
+//
+// const todo1 = {
+//   title: "organize desk",
+//   description: "clear clutter",
+// };
+//
+// const todo2 = updateTodo(todo1, {
+//   description: "throw out trash",
+// });
+//
+// console.log(todo1);
+// console.log(todo2);
 
 interface Props {
   a?: number;
@@ -27,3 +27,29 @@ interface Props {
 const obj: Props = { a: 5 };
 
 // const obj2: Required<Props> = { a: 5 };
+
+interface Todo {
+  title: string;
+}
+
+const todo: Readonly<Todo> = {
+  title: "Delete inactive users",
+};
+
+// todo.title = "Hello";
+console.log(todo)
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+type CatName = "miffy" | "boris" | "mordred";
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+};
+
+console.log(cats.boris);
