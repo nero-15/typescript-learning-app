b92f6fbb68249d88e524932b16b49b62306a892c
nero-15 <nero15.dev@gmail.com>
2021-08-17 21:57:50 +0900

    fix

--- Reference/enums.js
+++ Reference/enums.js
@@ -18,3 +18,17 @@ var BooleanLikeHeterogeneousEnum;
     BooleanLikeHeterogeneousEnum["Yes"] = "YES";
 })(BooleanLikeHeterogeneousEnum || (BooleanLikeHeterogeneousEnum = {}));
 console.log(BooleanLikeHeterogeneousEnum);
+var E1;
+(function (E1) {
+    E1[E1["X"] = 0] = "X";
+    E1[E1["Y"] = 1] = "Y";
+    E1[E1["Z"] = 2] = "Z";
+})(E1 || (E1 = {}));
+var E2;
+(function (E2) {
+    E2[E2["A"] = 1] = "A";
+    E2[E2["B"] = 2] = "B";
+    E2[E2["C"] = 3] = "C";
+})(E2 || (E2 = {}));
+console.log(E1);
+console.log(E2);
--- Reference/enums.ts
+++ Reference/enums.ts
@@ -19,3 +19,17 @@ enum BooleanLikeHeterogeneousEnum {
   Yes = "YES",
 }
 console.log(BooleanLikeHeterogeneousEnum);
+
+enum E1 {
+  X,
+  Y,
+  Z,
+}
+
+enum E2 {
+  A = 1,
+  B,
+  C,
+}
+console.log(E1);
+console.log(E2);
