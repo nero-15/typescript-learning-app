4b337a7798361fdb1b9e51cbefc28a436965b84d
nero-15 <nero15.dev@gmail.com>
2021-08-19 22:18:44 +0900

    Alternative Pattern

--- Reference/mixins.js
+++ Reference/mixins.js
@@ -1,55 +1,93 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+// class Sprite {
+//   name = "";
+//   x = 0;
+//   y = 0;
+//
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+// To get started, we need a type which we'll use to extend
+// other classes from. The main responsibility is to declare
+// that the type being passed in is a class.
+// type Constructor = new (...args: any[]) => {};
+// This mixin adds a scale property, with getters and setters
+// for changing it with an encapsulated private property:
+// function Scale<TBase extends Constructor>(Base: TBase) {
+//   return class Scaling extends Base {
+//     // Mixins may not declare private/protected properties
+//     // however, you can use ES2020 private fields
+//     _scale = 1;
+//
+//     setScale(scale: number) {
+//       this._scale = scale;
+//     }
+//
+//     get scale(): number {
+//       return this._scale;
+//     }
+//   };
+// }
+// Compose a new class from the Sprite class,
+// with the Mixin Scale applier:
+// const EightBitSprite = Scale(Sprite);
+//
+// const flappySprite = new EightBitSprite("Bird");
+// flappySprite.setScale(0.8);
+// console.log(flappySprite.scale);
+// This was our previous constructor:
+// type Constructor = new (...args: any[]) => {};
+// // Now we use a generic version which can apply a constraint on
+// // the class which this mixin is applied to
+// type GConstructor<T = {}> = new (...args: any[]) => T;
+//
+// type Positionable = GConstructor<{ setPos: (x: number, y: number) => void }>;
+// type Spritable = GConstructor<Sprite>;
+// type Loggable = GConstructor<{ print: () => void }>;
+//
+// function Jumpable<TBase extends Positionable>(Base: TBase) {
+//   return class Jumpable extends Base {
+//     jump() {
+//       // This mixin will only work if it is passed a base
+//       // class which has setPos defined because of the
+//       // Positionable constraint.
+//       this.setPos(0, 20);
+//     }
+//   };
+// }
+// Each mixin is a traditional ES class
+var Jumpable = /** @class */ (function () {
+    function Jumpable() {
+    }
+    Jumpable.prototype.jump = function () { };
+    return Jumpable;
+}());
+var Duckable = /** @class */ (function () {
+    function Duckable() {
+    }
+    Duckable.prototype.duck = function () { };
+    return Duckable;
+}());
+// Including the base
 var Sprite = /** @class */ (function () {
-    function Sprite(name) {
-        this.name = "";
+    function Sprite() {
         this.x = 0;
         this.y = 0;
-        this.name = name;
     }
     return Sprite;
 }());
-// This mixin adds a scale property, with getters and setters
-// for changing it with an encapsulated private property:
-function Scale(Base) {
-    return /** @class */ (function (_super) {
-        __extends(Scaling, _super);
-        function Scaling() {
-            var _this = _super !== null && _super.apply(this, arguments) || this;
-            // Mixins may not declare private/protected properties
-            // however, you can use ES2020 private fields
-            _this._scale = 1;
-            return _this;
-        }
-        Scaling.prototype.setScale = function (scale) {
-            this._scale = scale;
-        };
-        Object.defineProperty(Scaling.prototype, "scale", {
-            get: function () {
-                return this._scale;
-            },
-            enumerable: false,
-            configurable: true
+// Apply the mixins into the base class via
+// the JS at runtime
+applyMixins(Sprite, [Jumpable, Duckable]);
+var player = new Sprite();
+player.jump();
+console.log(player.x, player.y);
+// This can live anywhere in your codebase:
+function applyMixins(derivedCtor, constructors) {
+    constructors.forEach(function (baseCtor) {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(function (name) {
+            Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
+                Object.create(null));
         });
-        return Scaling;
-    }(Base));
+    });
 }
-// Compose a new class from the Sprite class,
-// with the Mixin Scale applier:
-var EightBitSprite = Scale(Sprite);
-var flappySprite = new EightBitSprite("Bird");
-flappySprite.setScale(0.8);
-console.log(flappySprite.scale);
--- Reference/mixins.ts
+++ Reference/mixins.ts
@@ -1,12 +1,12 @@
-class Sprite {
-  name = "";
-  x = 0;
-  y = 0;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+// class Sprite {
+//   name = "";
+//   x = 0;
+//   y = 0;
+//
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
 
 // To get started, we need a type which we'll use to extend
 // other classes from. The main responsibility is to declare
@@ -16,47 +16,88 @@ class Sprite {
 // This mixin adds a scale property, with getters and setters
 // for changing it with an encapsulated private property:
 
-function Scale<TBase extends Constructor>(Base: TBase) {
-  return class Scaling extends Base {
-    // Mixins may not declare private/protected properties
-    // however, you can use ES2020 private fields
-    _scale = 1;
-
-    setScale(scale: number) {
-      this._scale = scale;
-    }
-
-    get scale(): number {
-      return this._scale;
-    }
-  };
-}
+// function Scale<TBase extends Constructor>(Base: TBase) {
+//   return class Scaling extends Base {
+//     // Mixins may not declare private/protected properties
+//     // however, you can use ES2020 private fields
+//     _scale = 1;
+//
+//     setScale(scale: number) {
+//       this._scale = scale;
+//     }
+//
+//     get scale(): number {
+//       return this._scale;
+//     }
+//   };
+// }
 
 // Compose a new class from the Sprite class,
 // with the Mixin Scale applier:
-const EightBitSprite = Scale(Sprite);
-
-const flappySprite = new EightBitSprite("Bird");
-flappySprite.setScale(0.8);
-console.log(flappySprite.scale);
+// const EightBitSprite = Scale(Sprite);
+//
+// const flappySprite = new EightBitSprite("Bird");
+// flappySprite.setScale(0.8);
+// console.log(flappySprite.scale);
 
 // This was our previous constructor:
-type Constructor = new (...args: any[]) => {};
-// Now we use a generic version which can apply a constraint on
-// the class which this mixin is applied to
-type GConstructor<T = {}> = new (...args: any[]) => T;
+// type Constructor = new (...args: any[]) => {};
+// // Now we use a generic version which can apply a constraint on
+// // the class which this mixin is applied to
+// type GConstructor<T = {}> = new (...args: any[]) => T;
+//
+// type Positionable = GConstructor<{ setPos: (x: number, y: number) => void }>;
+// type Spritable = GConstructor<Sprite>;
+// type Loggable = GConstructor<{ print: () => void }>;
+//
+// function Jumpable<TBase extends Positionable>(Base: TBase) {
+//   return class Jumpable extends Base {
+//     jump() {
+//       // This mixin will only work if it is passed a base
+//       // class which has setPos defined because of the
+//       // Positionable constraint.
+//       this.setPos(0, 20);
+//     }
+//   };
+// }
 
-type Positionable = GConstructor<{ setPos: (x: number, y: number) => void }>;
-type Spritable = GConstructor<Sprite>;
-type Loggable = GConstructor<{ print: () => void }>;
 
-function Jumpable<TBase extends Positionable>(Base: TBase) {
-  return class Jumpable extends Base {
-    jump() {
-      // This mixin will only work if it is passed a base
-      // class which has setPos defined because of the
-      // Positionable constraint.
-      this.setPos(0, 20);
-    }
-  };
+// Each mixin is a traditional ES class
+class Jumpable {
+  jump() {}
+}
+
+class Duckable {
+  duck() {}
+}
+
+// Including the base
+class Sprite {
+  x = 0;
+  y = 0;
+}
+
+// Then you create an interface which merges
+// the expected mixins with the same name as your base
+interface Sprite extends Jumpable, Duckable {}
+// Apply the mixins into the base class via
+// the JS at runtime
+applyMixins(Sprite, [Jumpable, Duckable]);
+
+let player = new Sprite();
+player.jump();
+console.log(player.x, player.y);
+
+// This can live anywhere in your codebase:
+function applyMixins(derivedCtor: any, constructors: any[]) {
+  constructors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
+          Object.create(null)
+      );
+    });
+  });
 }
