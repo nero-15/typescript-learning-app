839f161f1ed640def651e9a6495d10aa8ded5ae4
nero-15 <nero15.dev@gmail.com>
2021-08-20 22:33:05 +0900

    fix

--- Reference/type_compatibility.js
+++ Reference/type_compatibility.js
@@ -3,3 +3,12 @@ function greet(pet) {
     console.log("Hello, " + pet.name);
 }
 greet(dog); // OK
+var x = function (a) { return 0; };
+var y = function (b, s) { return 0; };
+y = x; // OK
+// x = y; // Error
+var items = [1, 2, 3];
+// Don't force these extra parameters
+items.forEach(function (item, index, array) { return console.log(item); });
+// Should be OK!
+items.forEach(function (item) { return console.log(item); });
