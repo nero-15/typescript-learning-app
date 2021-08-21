b1a4fa08f68d76340f588c91344f483761723f75
nero-15 <nero15.dev@gmail.com>
2021-08-21 11:59:09 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { sayHello } from "./greet";
-console.log(sayHello("TypeScript"));
-
-function hello(compiler: string) {
-  console.log(`Hello from ${compiler}`);
+function showHello(divName: string, name: string) {
+  const elt = document.getElementById(divName);
+  elt.innerText = sayHello(name);
 }
-hello("TypeScript");
+showHello("greeting", "TypeScript");
