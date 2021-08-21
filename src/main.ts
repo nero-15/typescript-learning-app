5a9b0048def7817bf502d6252efb0a78715e8428
nero-15 <nero15.dev@gmail.com>
2021-08-21 12:09:12 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,4 @@ function showHello(divName: string, name: string) {
   const elt = document.getElementById(divName);
   elt.innerText = sayHello(name);
 }
-showHello("greeting", "TypeScript");
+showHello("greeting", "TypeScript!!");
